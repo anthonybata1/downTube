939824d3af306c89356ecc7e166f6e2e84f361d7
Anthony Correia <anty56.com@gmail.com>
2022-05-30 17:30:57 -0300

    Tentativa de criar pastas e melhorias no codigo

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
-# downTube pre-alpha v1.0.0 ~AnthonyBata1
+# downTube pre-alpha v0.0.2 ~AnthonyBata1
 Baixe vídeos e áudios do YouTube usando Python em melhor qualidade.
 
-Primeiro projeto postado aqui no GitHub, a ideia surgiu de madrugada, quando me lembrei das complicações que tive para baixar vídeo ou áudio do youtube.
+Primeiro projeto postado aqui no GitHub, a ideia surgiu de madrugada, quando me lembrei das complicações que tive para baixar vídeo ou áudio do YouTube.
 
-Você pode usá-lo à vontade, pode até salvá-lo algum dia. : D
+Você pode usá-lo à vontade, pode até salvá-lo algum dia. :D
 
 
 ## Requisitos:
-  Ter instalado no pip Pytube e Pymovie.
-    Caso não tenha segue os comando para por no terminal. (logo criarei um executável que funcione)
-      "pip install pytube"
-      "pip install pymovie"
-      
+Ter instalado no pip PyTube e MoviePy.
+Caso não tenha segue os comando para por no terminal. (logo criarei um executável que funcione)
+  ```bash
+$ pip install pytube
+
+$ pip install moviepy
+  ```
+
 ## Bugs:
-  Alguams vezes o Download demora muito, então tenha que fechar e abrir o programa novamente (ainda estou verificando este problema).
-  Não sei se tem outro.
+Algumas vezes o Download demora muito, então tenha que fechar e abrir o programa novamente (ainda estou verificando este problema).
+
+Não sei se tem outro.
--- downTube.py
+++ downTube.py
@@ -1,4 +1,6 @@
 from ctypes import windll
+from pathlib import Path
+from time import sleep
 from pytube import YouTube
 import moviepy.editor as mp
 from re import search
@@ -6,11 +8,18 @@ import os
 #coding: utf-8
 
 #definindo nome de titulo
-windll.kernel32.SetConsoleTitleW("downTube v0.0.1  ~Anthony")
+windll.kernel32.SetConsoleTitleW("downTube v0.0.2  ~Anthony")
 
 escolha = True
 
+#criando as pastas de download
+video_path = './downloads'
+mp3_path1 = './downloads/mp3'
+
+
+#programa
 while escolha:
+    
     #pedido do while
     os.system("cls || clear")
     print("\nO que deseja fazer?\n")
@@ -20,8 +29,8 @@ while escolha:
     escolha = input("Qual sua opção? ")
 
     if escolha == "1":
-        link = input("\nDigite a URL a ser baixada:\n")
-        path = input("\nDigite o local para o Download (ex: 'C:/download'):\n ")
+        link = input("\n\nDigite a URL a ser baixada:\n")
+        #path = input("\nDigite o local para o Download (ex: 'C:/download'): ")
         yt = YouTube(link)
 
         #detalhando video
@@ -31,15 +40,19 @@ while escolha:
         confirm = input("\nÉ este é a URL certa?(s/n) ")
         if confirm == "s":
             ys = yt.streams.get_highest_resolution()
+    
+            print("Baixando, aguarde...")
+            ys.download(video_path)
+            print("\nDownload Completo!!\n\n")
+            input("Aperte qualquer tecla para continuar...")
         
-        print("Baixando, aguarde...")
-        ys.download(path)
-        print("\nDownload Completo!!\n\n")
-        input("Aperte qualquer tecla para continuar...")
+        else:
+            print("Tente Novamente")
+            input("Aperte qualquer tecla para continuar...")
 
     if escolha == "2":
         link = input("\nDigite a URL a ser baixada:\n")
-        path = input("\nDigite o local para o Download (ex: 'C:/download'):\n ")
+        #path = input("\nDigite o local para o Download (ex: 'C:/download'): ")
         yt = YouTube(link)
 
         #detalhando video
@@ -49,23 +62,29 @@ while escolha:
         confirm = input("\nÉ este é a URL certa?(s/n) ")   
         if confirm == "s":
             print("Baixando, aguarde...")
-            ys = yt.streams.filter(only_audio=True).first().download(path)
-        print("\nDownload Completo!!\n\n")
-        
-        print('Convertendo arquivo...')
-        for file in os.listdir(path):
-            if search('mp4',file):
-                mp4_path = os.path.join(path, file)
-                mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
-                new_file = mp.AudioFileClip(mp4_path)
-                new_file.write_audiofile(mp3_path)
-                os.remove(mp4_path)
-        
-        print("\nSucesso!\n\n")
-        input("Aperte qualquer tecla para continuar...")
+            ys = yt.streams.filter(only_audio=True).first().download(mp3_path1)
+            print("\nDownload Completo!!\n\n")
+            
+            print('Convertendo arquivo...')
+            for file in os.listdir(mp3_path1):
+                if search('mp4',file):
+                    mp4_path = os.path.join(mp3_path1, file)
+                    mp3_path1 = os.path.join(mp3_path1, os.path.splitext(file)[0]+'.mp3')
+                    new_file = mp.AudioFileClip(mp4_path)
+                    new_file.write_audiofile(mp3_path1)
+                    os.remove(mp4_path)
+            
+            print("\nSucesso!\n\n")
+            input("Aperte qualquer tecla para continuar...")
+
+        else:
+            print("Tente Novamente")
+            input("Aperte qualquer tecla para continuar...")
 
     if escolha == "3":
-        break
+        print("Saindo do Programa...")
+        sleep(2.5)
+        exit()
 
 else:
     print("Erro!!")
