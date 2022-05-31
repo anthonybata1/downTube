6cd763f73d8b25447622b8327cade5df9956773e
Anthony Correia <anty56.com@gmail.com>
2022-05-31 05:11:42 -0300

    atualização de variavel

--- downTube.py
+++ downTube.py
@@ -66,9 +66,9 @@ while escolha:
             for file in os.listdir(mp3_path1):
                 if search('mp4',file):
                     mp4_path = os.path.join(mp3_path1, file)
-                    mp3_path1 = os.path.join(mp3_path1, os.path.splitext(file)[0]+'.mp3')
+                    mp3_path = os.path.join(mp3_path1, os.path.splitext(file)[0]+'.mp3')
                     new_file = mp.AudioFileClip(mp4_path)
-                    new_file.write_audiofile(mp3_path1)
+                    new_file.write_audiofile(mp3_path)
                     os.remove(mp4_path)
 
             print("\nSucesso!\n\n")
