5fddbf0ffa04ba4365a26ad4835ae2454e64edc3
Anthony Correia <anty56.com@gmail.com>
2022-05-31 05:08:59 -0300

    Remoção de códigos não utilizados e melhorais de texto

--- downTube.py
+++ downTube.py
@@ -1,5 +1,4 @@
 from ctypes import windll
-from pathlib import Path
 from time import sleep
 from pytube import YouTube
 import moviepy.editor as mp
@@ -12,14 +11,14 @@ windll.kernel32.SetConsoleTitleW("downTube v0.0.2  ~Anthony")
 
 escolha = True
 
-#criando as pastas de download
+#pastas de download na raiz do app
 video_path = './downloads'
 mp3_path1 = './downloads/mp3'
 
 
 #programa
 while escolha:
-    
+
     #pedido do while
     os.system("cls || clear")
     print("\nO que deseja fazer?\n")
@@ -30,7 +29,6 @@ while escolha:
 
     if escolha == "1":
         link = input("\n\nDigite a URL a ser baixada:\n")
-        #path = input("\nDigite o local para o Download (ex: 'C:/download'): ")
         yt = YouTube(link)
 
         #detalhando video
@@ -40,19 +38,18 @@ while escolha:
         confirm = input("\nÉ este é a URL certa?(s/n) ")
         if confirm == "s":
             ys = yt.streams.get_highest_resolution()
-    
+
             print("Baixando, aguarde...")
             ys.download(video_path)
             print("\nDownload Completo!!\n\n")
             input("Aperte qualquer tecla para continuar...")
-        
+
         else:
-            print("Tente Novamente")
+            print("\n\nTente Novamente")
             input("Aperte qualquer tecla para continuar...")
 
     if escolha == "2":
         link = input("\nDigite a URL a ser baixada:\n")
-        #path = input("\nDigite o local para o Download (ex: 'C:/download'): ")
         yt = YouTube(link)
 
         #detalhando video
@@ -64,7 +61,7 @@ while escolha:
             print("Baixando, aguarde...")
             ys = yt.streams.filter(only_audio=True).first().download(mp3_path1)
             print("\nDownload Completo!!\n\n")
-            
+
             print('Convertendo arquivo...')
             for file in os.listdir(mp3_path1):
                 if search('mp4',file):
@@ -73,19 +70,15 @@ while escolha:
                     new_file = mp.AudioFileClip(mp4_path)
                     new_file.write_audiofile(mp3_path1)
                     os.remove(mp4_path)
-            
+
             print("\nSucesso!\n\n")
             input("Aperte qualquer tecla para continuar...")
 
         else:
-            print("Tente Novamente")
+            print("\n\nTente Novamente")
             input("Aperte qualquer tecla para continuar...")
 
     if escolha == "3":
-        print("Saindo do Programa...")
+        print("\n\nSaindo do Programa...")
         sleep(2.5)
-        exit()
-
-else:
-    print("Erro!!")
-    input("Aperte qualquer tecla para continuar...")
+        break
